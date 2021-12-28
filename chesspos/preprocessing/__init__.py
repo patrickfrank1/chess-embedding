141ce021a7f61a8ca827eaca7be3211fc450f6aa
Patrick Frank <patr.frank@gmx.de>
2021-12-28 17:15:47 +0100

    Initial transfer from old repo

---
+++ chesspos/preprocessing/__init__.py
@@ -0,0 +1,3 @@
+from chesspos.preprocessing.sample_generator import SampleGenerator
+from chesspos.preprocessing.bitboard_preprocessor import *
+from chesspos.preprocessing.board_bitboard_converter import *
---
+++ chesspos/test/test_convert.py
@@ -0,0 +1,22 @@
+import numpy as np
+import chess
+
+from chesspos.preprocessing.board_bitboard_converter import board_to_bitboard, bitboard_to_board
+
+start_board = chess.Board(chess.STARTING_FEN)
+start_bb = np.load("./chesspos/test/startpos.npy")
+
+def test_board_to_bitboard():
+	board_result = board_to_bitboard(start_board)
+	assert board_result.shape == (773,)
+	assert board_result.dtype == 'bool'
+	assert np.all(board_result == start_bb)
+
+def test_full_conversion():
+	start_bitboard = board_to_bitboard(start_board)
+	reconstructed_board = bitboard_to_board(start_bitboard)
+	print(reconstructed_board.__str__())
+	assert start_board.board_fen() == reconstructed_board.board_fen()
+
+if __name__ == "__main__":
+	test_full_conversion()

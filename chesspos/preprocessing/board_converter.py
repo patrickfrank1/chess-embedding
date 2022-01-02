e2da284cda464c1d15d9a432907c32419d2cad0a
Patrick Frank <patr.frank@gmx.de>
2022-01-02 17:39:29 +0100

    Add converters for tensor representation of board

--- chesspos/preprocessing/board_converter.py
+++ chesspos/preprocessing/board_converter.py
@@ -11,10 +11,10 @@ def board_to_bitboard(board):
 			embedding = np.concatenate((embedding, bmp))
 	additional = np.array([
 		bool(board.turn),
-		bool(board.castling_rights & chess.BB_A1),
-		bool(board.castling_rights & chess.BB_H1),
-		bool(board.castling_rights & chess.BB_A8),
-		bool(board.castling_rights & chess.BB_H8)
+		board.has_kingside_castling_rights(chess.WHITE),
+		board.has_queenside_castling_rights(chess.WHITE),
+		board.has_kingside_castling_rights(chess.BLACK),
+		board.has_queenside_castling_rights(chess.BLACK)
 	])
 	embedding = np.concatenate((embedding, additional))
 	return embedding
@@ -26,7 +26,7 @@ def bitboard_to_board(bb):
 	# loop over all pieces and squares
 	for color in [1, 0]: # white, black
 		for i in range(1, 7): # P N B R Q K
-			idx = (1-color)*6+i-1
+			idx = (1-color)*6 + i - 1
 			piece = chess.Piece(i,color)
 
 			bitmask = bb[idx*64:(idx+1)*64]
@@ -50,3 +50,71 @@ def bitboard_to_board(bb):
 	reconstructed_board.set_castling_fen(castling_rights)
 
 	return reconstructed_board
+
+def board_to_tensor(board):
+	embedding = np.empty((8,8,0), dtype=bool)
+	# one plane per piece
+	for color in [1, 0]:
+		for i in range(1, 7): # P N B R Q K / white
+			bmp = np.zeros(shape=(64,)).astype(bool)
+			for j in list(board.pieces(i, color)):
+				bmp[j] = True
+			bmp = bmp.reshape((8,8,1))
+			embedding = np.concatenate((embedding, bmp), axis=2)
+
+	# castling rights at plane embedding(:,:,12)
+	castling_rights = np.zeros((8,8,1), dtype=bool)
+	castling_rights[0,0,0] = board.has_queenside_castling_rights(chess.WHITE)
+	castling_rights[0,7,0] = board.has_kingside_castling_rights(chess.WHITE)
+	castling_rights[7,0,0] = board.has_queenside_castling_rights(chess.BLACK)
+	castling_rights[7,7,0] = board.has_kingside_castling_rights(chess.BLACK)
+	embedding = np.concatenate((embedding, castling_rights), axis=2)
+
+	# en passant squares at plane embedding(:,:,13)
+	en_passant = np.zeros((64,), dtype=bool)
+	if board.has_legal_en_passant():
+		en_passant[board.ep_square] = True
+	en_passant = en_passant.reshape((8,8,1))
+	embedding = np.concatenate((embedding, en_passant), axis=2)
+
+	# turn at plane embedding(:,:,14)
+	turn = np.zeros((8,8,1), dtype=bool)
+	turn[0,0,0] = board.turn
+	embedding = np.concatenate((embedding, turn), axis=2)
+
+	return embedding
+
+def tensor_to_board(tensor):
+	assert tensor.shape == (8,8,15)
+
+	# set up empty board
+	reconstructed_board = chess.Board()
+	reconstructed_board.clear()
+
+	# loop over all pieces and squares
+	for color in [1, 0]: # white, black
+		for i in range(1, 7): # P N B R Q K
+			idx = (1-color)*6 + i - 1
+			piece = chess.Piece(i,color)
+			square_bitmask = tensor[:,:,idx].reshape((64,))
+			squares = np.flatnonzero(square_bitmask)
+			for square in squares:
+				reconstructed_board.set_piece_at(square,piece)
+	
+	# set castling rights
+	castling_rights = ''
+	if tensor[0,0,12]: castling_rights += 'Q'
+	if tensor[0,7,12]: castling_rights += 'K'
+	if tensor[7,0,12]: castling_rights += 'q'
+	if tensor[7,7,12]: castling_rights += 'k'
+	reconstructed_board.set_castling_fen(castling_rights)
+
+	# set en passant square
+	en_passant = tensor[:,:,13].reshape((64,))
+	if np.any(en_passant):
+		reconstructed_board.ep_square = np.flatnonzero(en_passant)[0]
+
+	# set turn
+	reconstructed_board.turn = tensor[0,0,14]
+
+	return reconstructed_board			
--- chesspos/test/test_convert.py
+++ chesspos/test/test_convert.py
@@ -1,22 +1,39 @@
 import numpy as np
 import chess
 
-from chesspos.preprocessing.board_converter import board_to_bitboard, bitboard_to_board
+from chesspos.preprocessing.board_converter import (
+	board_to_bitboard, bitboard_to_board, board_to_tensor, tensor_to_board
+)
 
 start_board = chess.Board(chess.STARTING_FEN)
-start_bb = np.load("./chesspos/test/startpos.npy")
+ep_board = chess.Board("rnbqkbnr/pp1p1ppp/8/2pPp3/8/8/PPP1PPPP/RNBQKBNR w KQkq c6 0 3")
 
 def test_board_to_bitboard():
 	board_result = board_to_bitboard(start_board)
 	assert board_result.shape == (773,)
 	assert board_result.dtype == 'bool'
-	assert np.all(board_result == start_bb)
 
-def test_full_conversion():
+def test_full_bitboard_conversion():
 	start_bitboard = board_to_bitboard(start_board)
 	reconstructed_board = bitboard_to_board(start_bitboard)
 	print(reconstructed_board.__str__())
 	assert start_board.board_fen() == reconstructed_board.board_fen()
 
+def test_board_to_tensor():
+	start_tensor = board_to_tensor(start_board)
+	assert start_tensor.shape == (8, 8, 15)
+	assert start_tensor.dtype == 'bool'
+
+def test_board_to_tensor_ep():
+	ep_tensor = board_to_tensor(ep_board)
+	assert ep_tensor[5,2,13] == True
+
+def test_full_tensor_conversion():
+	ep_tensor = board_to_tensor(ep_board)
+	reconstructed_board = tensor_to_board(ep_tensor)
+	print(reconstructed_board.__str__())
+	assert ep_board.board_fen() == reconstructed_board.board_fen()
+
 if __name__ == "__main__":
-	test_full_conversion()
+	test_full_bitboard_conversion()
+	test_full_tensor_conversion()

658575bfadfefd2697c146e033bd8488c9ab46b6
patrickfrank <25801668+patrickfrank1@users.noreply.github.com>
2022-01-15 16:48:55 +0100

    Refactor sample generator (#1)

--- chesspos/models/cnn_resnet_autoencoder.py
+++ chesspos/models/cnn_resnet_autoencoder.py
@@ -1,3 +1,4 @@
+import numpy as np
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
@@ -37,46 +38,60 @@ class CnnResnetAutoencoder(TrainableModel, ChesspositionInspectableAutoencoderMi
 		self.build_model()
 
 	def build_model(self):
-		input_layer = layers.Input(shape=773, dtype=tf.float16)
+		# build encoder
+		input_layer = layers.Input(shape=(8,8,15,1), dtype=tf.float16)
 
-		board_layer = layers.Lambda(lambda x: x[:,:768], output_shape=(768,))(input_layer)
-		board_layer = layers.Reshape((8,8,12))(board_layer)
-
-		metadata_layer = layers.Lambda(lambda x: x[:,768:], output_shape=(5,))(input_layer)
-
+		conv_1 = layers.Conv3D(8, (2,2,15), activation="relu", padding="same")(input_layer)
+		conv_1 = layers.BatchNormalization()(conv_1)
+		conv_2 = layers.Conv3D(16, (2,2,15), activation="relu", padding="same")(conv_1)
+		conv_2 = layers.BatchNormalization()(conv_2)
+		conv_3 = layers.Conv3D(32, (2,2,15), activation="relu", padding="same")(conv_2)
+		conv_3 = layers.BatchNormalization()(conv_3)
+		#conv_4 = layers.Conv3D(64, (2,2,15), activation="relu", padding="same")(conv_3)
+		#conv_4 = layers.BatchNormalization()(conv_4)
+		#conv_5 = layers.Conv3D(128, (2,2,15), activation="relu", padding="same")(conv_4)
+		#conv_5 = layers.BatchNormalization()(conv_5)
+		#conv_6 = layers.Conv3D(256, (2,2,15), activation="relu", padding="same")(conv_5)
+		#conv_6 = layers.BatchNormalization()(conv_6)
+		#conv_7 = layers.Conv3D(512, (2,2,15), activation="relu", padding="same")(conv_6)
+		#conv_7 = layers.BatchNormalization()(conv_7)
 		
-		encoder = layers.Conv2D(self.filters, (3, 3), activation="relu", padding="same")(board_layer)
-		encoder_1 = layers.BatchNormalization()(encoder)
-		encoder = layers.Conv2D(self.filters, (3, 3), activation="relu", padding="same")(encoder_1)
-		encoder = layers.BatchNormalization()(encoder) + encoder_1
-		encoder = layers.Flatten()(encoder)
-		encoder = layers.Dense(128, activation="relu")(encoder)
-		encoder = layers.Dropout(0.3)(encoder)
-		encoder = layers.Dense(64, activation="relu")(encoder)
-		
+		final_conv_shape = conv_3.shape[1:]
+		print("final_conv_shape:", final_conv_shape)
+		encoder = layers.Flatten()(conv_3)
+		encoder = layers.Dense(self.embedding_size, activation="relu")(encoder)
+
 		encoder_model = keras.Model(inputs=input_layer, outputs=encoder, name='encoder')
 		encoder_model.summary()
 		self.encoder = encoder_model
 
-		decoder_input = layers.Input(shape=64)
-		decoder = layers.Dense(128, activation="relu")(decoder_input)
-		decoder = layers.Dropout(0.3)(decoder)
-		decoder = layers.Dense(8192, activation="relu")(decoder)
-		decoder_1 = layers.Reshape((8,8,self.filters))(decoder)
-		decoder = layers.Conv2DTranspose(self.filters, (3, 3), activation="relu", padding="same")(decoder_1)
-		decoder = layers.BatchNormalization()(decoder) + decoder_1
-		decoder = layers.Conv2DTranspose(12, (3, 3), activation="relu", padding="same")(decoder)
-		decoder = layers.Reshape((768,))(decoder)
+		# build decoder
+		decoder_input = layers.Input(shape=(self.embedding_size,))
+		decoder_dense = layers.Dense(np.prod(final_conv_shape), activation="relu")(decoder_input)
 
-		decoder_model = keras.Model(inputs=decoder_input, outputs=decoder, name='decoder')
+		deconv_1 = layers.Reshape(final_conv_shape)(decoder_dense)
+		#deconv_1 = layers.Conv3DTranspose(256, (2,2,15), activation="relu", padding="same")(deconv_1)
+		#deconv_1 = layers.BatchNormalization()(deconv_1)
+		#deconv_2 = layers.Conv3DTranspose(128, (2,2,15), activation="relu", padding="same")(deconv_1)
+		#deconv_2 = layers.BatchNormalization()(deconv_2)
+		#deconv_3 = layers.Conv3DTranspose(64, (2,2,15), activation="relu", padding="same")(deconv_2)
+		#deconv_3 = layers.BatchNormalization()(deconv_3)
+		#deconv_4 = layers.Conv3DTranspose(32, (2,2,15), activation="relu", padding="same")(deconv_3)
+		#deconv_4 = layers.BatchNormalization()(deconv_4)
+		deconv_5 = layers.Conv3DTranspose(16, (2,2,15), activation="relu", padding="same")(deconv_1)
+		deconv_5 = layers.BatchNormalization()(deconv_5)
+		deconv_6 = layers.Conv3DTranspose(8, (2,2,15), activation="relu", padding="same")(deconv_5)
+		deconv_6 = layers.BatchNormalization()(deconv_6)
+		deconv_7 = layers.Conv3DTranspose(1, (2,2,15), activation="sigmoid", padding="same")(deconv_6)
+
+		decoder_model = keras.Model(inputs=decoder_input, outputs=deconv_7, name='decoder')
 		decoder_model.summary()
 		self.decoder = decoder_model
 
 		autoencoder = encoder_model(input_layer)
 		autoencoder = decoder_model(autoencoder)
-		output_layer = layers.Concatenate()([autoencoder, metadata_layer])
 
-		autoencoder_model = keras.Model(inputs=input_layer, outputs=output_layer, name='autocoder')
+		autoencoder_model = keras.Model(inputs=input_layer, outputs=autoencoder, name='autocoder')
 		autoencoder_model.summary()
 		self.model = autoencoder_model
 
